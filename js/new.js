060a586647785703462009dc0b34657cc83ff6da
Siranush26 <siranush.tsaturyan.dwin@gmail.com>
2022-05-08 19:57:22 +0400

    "navbar changes"

--- js/new.js
+++ js/new.js
@@ -1,5 +1,14 @@
-$(document).ready(function() {
-    $('.block_title').click(function(event){
-        $(this).toggleClass('active').next().slideToggle(300);
-    });
-});
+let burger=document.querySelector("#hamburger")
+let nav_menu=document.querySelector("#nav_menu")
+
+burger.addEventListener("click", navbar);
+
+function navbar() {
+    nav_menu.classList.add("nav_menu");
+ 
+    if (nav_menu.style.display === "block") {
+        nav_menu.style.display = "none";
+      } else {
+        nav_menu.style.display = "block";
+      }
+}
